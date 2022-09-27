7a745ab888716aab5a9a41edc4f639814ba260f2
crewd <newspaper321@nate.com>
2022-09-28 06:45:04 +0900

    design: layout margin

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,6 +1,6 @@
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-[768px] w-full m-auto bg-white px-[40px] ">
+    <div className="max-w-[768px] w-full m-auto bg-white px-[40px] md:my-[40px] ">
       <header className="py-[20px] border-double border-b-4 border-gray-400">
         <h1 className="font-bold text-xxl">MyReceipt</h1>
         <p className="flex justify-end w-full text-gray-500">닉네임 님</p>
