8a36819a54af7584059e99d20527f638e42085b3
crewd <newspaper321@nate.com>
2022-09-28 05:07:55 +0900

    fix: tailwind.config.js 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Noto Sans KR", 'sans-serif']
+        sans: ['Noto Sans KR', 'sans-serif'],
+      },
+      fontSize: {
+        regular: '16px',
+        max: '40px',
+        xxl: '34px',
+        xl: '26px',
+        lg: '24px',
+        md: '20px',
+        sm: '12px',
+        xs: '10px',
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
-}
+  plugins: [require('tailwind-scrollbar-hide')],
+};
