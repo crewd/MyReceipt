eb4abf8cc3022f60180d8d1113e7f4c1e6af0ca0
crewd <newspaper321@nate.com>
2022-09-30 09:25:28 +0900

    design: 배치 수정

--- components/common/BreakDownCard.tsx
+++ components/common/BreakDownCard.tsx
@@ -11,18 +11,18 @@ const BreakDownCard = ({
 }) => {
   return (
     <div
-      className={`p-[20px] flex justify-between text-md ${
-        deleteContent ? '' : 'hover:bg-gray-100/80 cursor-pointer'
+      className={`p-[20px] text-md ${
+        deleteContent ? 'grid grid-cols-3' : 'hover:bg-gray-100/80 cursor-pointer flex justify-between'
       } `}>
       <div className="relative">
-        <h3>{title}</h3>
+        <h3 className="text-left">{title}</h3>
         {date && <div className="absolute bottom-[25px] left-0 text-xs text-gray-500">{date}</div>}
       </div>
-      <span className={`${price > 0 ? 'text-green-500' : 'text-red-500'}`}>
+      <span className={`${price > 0 ? 'text-green-500' : 'text-red-500'} text-center`}>
         {price > 0 ? `+${price.toLocaleString()}` : price.toLocaleString()}
       </span>
       {deleteContent && (
-        <button className="hover:text-red-500" onClick={deleteContent}>
+        <button className="text-right hover:text-red-500" onClick={deleteContent}>
           삭제
         </button>
       )}
