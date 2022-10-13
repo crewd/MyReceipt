2138b23ec5ee933bbad6ebe977cbd2a156b4ca94
crewd <newspaper321@nate.com>
2022-10-13 16:22:58 +0900

    fix: api 수정

--- pages/api/basicFunds.ts
+++ pages/api/basicFunds.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import data from '../../data/index.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+  if (req.method === 'POST' && req.body.basicFunds) {
     data.basicFunds = req.body.basicFunds;
     const basicFunds = data.basicFunds;
     return res.status(200).json({ basicFunds });
