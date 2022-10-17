80797101c8772b2f09fb756d5e77611f89b82ba6
crewd <newspaper321@nate.com>
2022-10-18 06:29:41 +0900

    design: padding 변경

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-[768px] sm:w-[calc(100%-80px)] w-full m-auto bg-white px-[40px] sm:my-[40px] sm:min-h-[calc(100vh-80px)] min-h-screen">
+    <div className="max-w-[768px] sm:w-[calc(100%-80px)] w-full m-auto bg-white md:px-[40px] px-[15px] sm:my-[40px] sm:min-h-[calc(100vh-80px)] min-h-screen">
       <header className="py-[20px] px-[20px] border-double border-b-4 border-gray-400">
         <h1 className="font-bold text-xxl">
           <Link href="/">
