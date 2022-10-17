440f09ade8da96ba8870ca1cb8c4553e8ca33e6c
crewd <newspaper321@nate.com>
2022-10-17 23:58:55 +0900

    fix: api query 수정

--- pages/api/item/[id].ts
+++ pages/api/item/[id].ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-// import defaultData from '../../../data/index.json';
+import defaultData from '../../../data/index.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const itemId = req.query;
-  // if (req.method === 'GET' && itemId) {
-  //   const detailItem = defaultData.Items[Number(itemId.id)];
-  //   return res.status(200).json(detailItem);
-  // }
-  return res.status(200).json({ success: true });
+  const { id } = req.query;
+  if (req.method === 'GET' && id) {
+    const detailItem = defaultData.Items[Number(id)];
+    return res.status(200).json(detailItem);
+  }
+  return res.status(400).json({ success: false });
 }
